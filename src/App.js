f12f9bd3d4073d80934bc51fe00992e82fd7a94e
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2021-07-10 15:09:57 +0200

    Refactor weather data fetch
    Changed from promise to async/await method
    Error code now throw error.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState, useRef } from 'react'
 export default function App() {
   const [weatherData, setWeatherData] = useState({});
   const cityNameRef = useRef();
+  /**
+   * Fetch data from middle api server and sets weather data
+   * If error uccures create notification to client
+   */
+  async function handleTownChange(e) {
+    if (e.key === "Enter") {
+      try {
+        const response = await (await fetch(`http://localhost:3000/${cityNameRef.current.value}`)).json();
 
-  function handleTownChange(e) {
-    if (e.key === "Enter") fetch(`http://localhost:3000/${cityNameRef.current.value}`)
-      .then(response => response.json())
-      .then((json) => {
-        if (json.cod !== 200) throw json;
-        setWeatherData(json)
-      })
-      .catch(error => console.log("error", error));// TODO Handle errors(popup modal or setState)
+        if (response.cod !== 200) throw response;
+        setWeatherData(response);
+      } catch (error) {
+        // TODO Handle errors(popup modal or setState)
+      }
+    }
   }
   return (
     <>
